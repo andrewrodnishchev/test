90b59544743c76cb6277624fd1a944af5bf8b9d2
rodnischev <rodnishev.andrei@mail.ru>
2025-03-10 12:30:10 +0300

    новая сборка

--- test.py
+++ test.py
@@ -16,11 +16,8 @@ class TestLogin:
         chrome_options.add_argument("--no-sandbox")  # Для CI/CD
         chrome_options.add_argument("--disable-dev-shm-usage")  # Для CI/CD
 
-        # Подключение к удаленному WebDriver
-        cls.driver = webdriver.Remote(
-            command_executor='http://localhost:4444/wd/hub',  # Убедитесь, что это правильный адрес
-            options=chrome_options
-        )
+        # Используем локальный WebDriver
+        cls.driver = webdriver.Chrome(options=chrome_options)
         cls.driver.implicitly_wait(10)
 
     @classmethod
@@ -42,11 +39,17 @@ class TestLogin:
 
     def perform_login(self, username, password):
         logging.info("Вводим имя пользователя")
-        self.wait_for_element(By.XPATH, '//*[@id="Email"]').send_keys(username)
+        email_field = self.wait_for_element(By.XPATH, '//*[@id="Email"]')
+        email_field.send_keys(username)
+
         logging.info("Вводим пароль")
-        self.wait_for_element(By.CSS_SELECTOR, '#PasswordUser ').send_keys(password)
+        password_field = self.wait_for_element(By.CSS_SELECTOR, '#PasswordUser ')
+        password_field.send_keys(password)
+
         logging.info("Нажимаем кнопку входа")
-        self.wait_for_element(By.XPATH, '/html/body/div[1]/div/div/div/form/button').click()
+        login_button = self.wait_for_element(By.XPATH, '/html/body/div[1]/div/div/div/form/button')
+        login_button.click()
 
     def wait_for_element(self, by, value):
+        logging.info(f"Ожидание элемента: {by} с значением: {value}")
         return WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((by, value)))
